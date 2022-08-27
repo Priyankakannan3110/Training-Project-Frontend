52c4eab3d9db0215c9e926f7dfeb59743fe833df
Priyanka <priyankakannan3110@gmail.com>
2022-08-27 17:56:15 +0530

    Initial Commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { SnackbarService } from '../services/snackbar.service';
+import { UserService } from '../services/user.service';
+import { GlobalConstants } from '../shared/global-constants';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  loginForm: any = FormGroup;
+  responseMessage: any;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private userService: UserService,
+    private snackBarService: SnackbarService,
+    private dialogRef: MatDialogRef<LoginComponent>
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: [
+        null,
+        [Validators.required, Validators.pattern(GlobalConstants.emailRegex)],
+      ],
+      password: [null, [Validators.required]],
+    });
+  }
+
+  handelSubmit() {
+    var formData = this.loginForm.value;
+    var data = {
+      email: formData.email,
+      password: formData.password,
+    };
+    this.userService.login(data).subscribe(
+      (response: any) => {
+        this.dialogRef.close();
+        localStorage.setItem('token', response.token);
+        this.router.navigate(['/restaurant/dashboard']);
+      },
+      (error) => {
+        if (error.error?.message) {
+          this.responseMessage = error.error?.message;
+        } else {
+          this.responseMessage = GlobalConstants.genericError;
+        }
+        this.snackBarService.openSnackBar(
+          this.responseMessage,
+          GlobalConstants.error
+        );
+      }
+    );
+  }
+}
